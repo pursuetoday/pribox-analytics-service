f29d6436651e23e8cd35a51e96e6a5957abb5509
hafiza-sabeeha-areej <hamzarehman@pursue.today>
2023-08-11 17:33:15 +0500

    hotfix: analytics

--- src/campaignAnalytics/analyticSchedule/analyticScheduler.js
+++ src/campaignAnalytics/analyticSchedule/analyticScheduler.js
@@ -1,8 +1,13 @@
-import moment from 'moment';
+// import moment from 'moment';
+import { map } from 'lodash';
 import Campaign from '../campaign';
-import { campaignAnalyticExecutionQueue, campaignAnalyticExecutionTitle } from '../queues';
-import { generateJobId } from '../../utils/jobId';
+// import { campaignAnalyticExecutionQueue, campaignAnalyticExecutionTitle } from '../queues';
+// import { generateJobId } from '../../utils/jobId';
 import log from '../../utils/log';
+import promiseLimit from 'promise-limit';
+import analyticExecutioner from './analyticExecution';
+
+const pLimit = promiseLimit(10);
 
 async function processAnalyticScheduler() {
 	const query = {
@@ -18,20 +23,22 @@ async function processAnalyticScheduler() {
 
 	log(`campaign analytic scheduler init campaignsLength ${campaigns?.length}`, { debug: true });
 
-	campaigns.forEach(async (campaign, i) => {
-		const analyticEndDate = moment(campaign?.duration?.endingAt).add(5, 'days');
+	await Promise.all(map(campaigns, (campaign) => pLimit(() => analyticExecutioner(campaign))));
 
-		if (moment().isBefore(analyticEndDate)) {
-			const jobId = generateJobId(campaignAnalyticExecutionTitle, campaign._id);
-			const currentJob = await campaignAnalyticExecutionQueue.getJob(jobId);
-			if (!currentJob) {
-				campaignAnalyticExecutionQueue.add(campaignAnalyticExecutionTitle, campaign, {
-					delay: 300 * i,
-					jobId,
-				});
-			}
-		}
-	});
+	// campaigns.forEach(async (campaign, i) => {
+	// 	const analyticEndDate = moment(campaign?.duration?.endingAt).add(5, 'days');
+
+	// 	if (moment().isBefore(analyticEndDate)) {
+	// 		const jobId = generateJobId(campaignAnalyticExecutionTitle, campaign._id);
+	// 		const currentJob = await campaignAnalyticExecutionQueue.getJob(jobId);
+	// 		if (!currentJob) {
+	// 			campaignAnalyticExecutionQueue.add(campaignAnalyticExecutionTitle, campaign, {
+	// 				delay: 300 * i,
+	// 				jobId,
+	// 			});
+	// 		}
+	// 	}
+	// });
 }
 
 async function analyticScheduler() {
